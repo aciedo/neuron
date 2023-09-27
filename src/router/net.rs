ac85c71eeb5c139e518299473b58b33829f4c360
BillVBennings <bill@valera.co>
2023-09-27 23:02:18 +0100

    sorry azz
    it not work :( me sadge. commiting so we can cry into it together :')

--- src/router/net.rs
+++ src/router/net.rs
@@ -1,5 +1,12 @@
-use std::{io, sync::Arc, net::SocketAddr};
+use std::{io, net::SocketAddr, sync::Arc};
 
+use quinn::ConnectError;
+use quinn::ConnectError::{InvalidDnsName, InvalidRemoteAddress};
+use quinn::ConnectionError::{ApplicationClosed, ConnectionClosed, TransportError};
+use quinn_proto::transport_parameters as QError;
+use rustls::Error as RlsError;
+
+use quinn::{ApplicationClose, ConnectionClose, ConnectionError};
 use rustls::ServerConfig;
 
 struct Endpoint {
@@ -8,14 +15,17 @@ struct Endpoint {
 }
 
 pub enum ConnectingError {
+    RustlsError(rustls::Error),
+
     EndpointStopping,
     TooManyConnections,
     InvalidDnsName(String),
     InvalidRemoteAddress(SocketAddr),
     NoDefaultClientConfig,
     UnsupportedVersion,
+
     VersionMismatch,
-    TransportError(Error),
+    TransportError(RlsError),
     ConnectionClosed(ConnectionClose),
     ApplicationClosed(ApplicationClose),
     Reset,
@@ -24,41 +34,53 @@ pub enum ConnectingError {
 }
 
 impl From<ConnectError> for ConnectingError {
-    use ConnectError as ce;
-    use ConnectionError as cte;
-    fn from(error: ConnectingError) -> Self {
+    fn from(error: ConnectError) -> Self {
         match error {
-            EndpointStopping => return ce:self,
-            TooManyConnections => return ce:self,
-            InvalidDnsName(String) => return ce:self,
-            InvalidRemoteAddress(SocketAddr) => return ce:self,
-            NoDefaultClientConfig => return ce:self,
-            UnsupportedVersion => return ce:self,
-            VersionMismatch => return cte:self,
-            TransportError(Error) => return cte:self,
-            ConnectionClosed(ConnectionClose) => return cte:self,
-            ApplicationClosed(ApplicationClose) => return cte:self,,
-            Reset => return cte:self,
-            TimedOut => return cte:self,
-            LocallyClosed => return cte:self,
+            EndpointStopping => return ConnectingError::EndpointStopping,
+            TooManyConnections => return ConnectingError::TooManyConnections,
+            InvalidDnsName(String) => return ConnectingError::InvalidDnsName(String),
+            InvalidRemoteAddress(SocketAddr) => {
+                return ConnectingError::InvalidRemoteAddress(SocketAddr)
+            }
+            NoDefaultClientConfig => return ConnectingError::NoDefaultClientConfig,
+            UnsupportedVersion => return ConnectingError::UnsupportedVersion,
         }
     }
 }
 
-
+impl From<ConnectionError> for ConnectingError {
+    fn from(error: ConnectionError) -> Self {
+        match error {
+            VersionMismatch => return ConnectingError::VersionMismatch,
+            TransportError(Error) => return ConnectingError::TransportError(RlsError),
+            ConnectionClosed(ConnectionClose) => {
+                return ConnectingError::ConnectionClosed(ConnectionClose)
+            }
+            ApplicationClosed(ApplicationClose) => {
+                return ConnectingError::ApplicationClosed(ApplicationClose)
+            }
+            Reset => return ConnectingError::Reset,
+            TimedOut => return ConnectingError::TimedOut,
+            LocallyClosed => return ConnectingError::LocallyClosed,
+        }
+    }
+}
 
 impl Endpoint {
     /// Creates a new QUIC endpoint bound to the given socket address with the given TLS configuration.
     pub fn new(
-        tls: ServerConfig, 
+        tls: ServerConfig,
         socket_addr: SocketAddr,
         client_config: Arc<rustls::ClientConfig>,
     ) -> io::Result<Self> {
         let server_config = quinn::ServerConfig::with_crypto(Arc::new(tls));
-        Ok(Endpoint { endpoint: quinn::Endpoint::server(server_config, socket_addr)?, client_config })
+        Ok(Endpoint {
+            endpoint: quinn::Endpoint::server(server_config, socket_addr)?,
+            client_config,
+        })
     }
-    
+
     pub fn connect(socket_addr: SocketAddr, server_name: &str) -> Result<(), ConnectingError> {
-        
+        todo!();
     }
 }
