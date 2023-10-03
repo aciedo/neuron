7778b074e9e229114dd03c68df9189820e2aeaeb
Azz <azz@aciedo.com>
2023-10-03 18:34:02 +0100

    added additional comments to clarify stream usage

--- src/router/mod.rs
+++ src/router/mod.rs
@@ -1,1 +1,22 @@
 mod net;
+
+// As consensus happens at a partition level rather than a topic level, each
+// partition gets its own task. NEURON's implementation considers locks to slow
+// down the system, so we try to reduce their use as much as possible. This task
+// exclusively controls the partition's state machine, and is responsible for
+// handling all messages.
+
+// Some functionality inside partitions is delegated to additional tasks, such
+// as sending topic updates to subscribers and tracking message delivery. This
+// ensures that the main state machine loop inside each partition doesn't get
+// blocked. The state machine loop can process much faster than the network can
+// deliver messages, so subscriber delivery is tracked on a per-publish basis by
+// the networking task and queued for delivery. A highly synchronous partition
+// core with delegated IO ensures extreme efficiency on a
+// partition-per-partition basis.
+
+// NEURON's architecture is designed to scale linearly per core, limited
+// ultimately by networking alone, given appropriate CPU cores for the number of
+// partitions. Theoretically, you could reach CPU frequency limits after this,
+// but this will likely be in the 80gbps+ per partition range. You'd have to be 
+// breaching 5Tbps+ per cluster to even approach this sort of limit.
--- src/router/net.rs
+++ src/router/net.rs
@@ -1,8 +1,15 @@
-use std::{io::{self, BufReader, Cursor}, net::SocketAddr, sync::Arc};
+use std::{
+    io::{self, BufReader, Cursor},
+    net::SocketAddr,
+    sync::Arc,
+};
 
-use quinn::{ConnectError, ConnectionError, default_runtime};
+use quinn::{default_runtime, ConnectError, ConnectionError};
 use quinn_proto::{ApplicationClose, ConnectionClose, TransportError};
-use rustls::{Certificate, internal::msgs::codec::Codec, PrivateKey, server::AllowAnyAuthenticatedClient};
+use rustls::{
+    internal::msgs::codec::Codec, server::AllowAnyAuthenticatedClient,
+    Certificate, PrivateKey,
+};
 
 const SKI_ROOT_CA: &[u8] = include_bytes!("ski.crt");
 
@@ -32,13 +39,21 @@ impl From<ConnectError> for ConnectingError {
     fn from(error: ConnectError) -> Self {
         match error {
             ConnectError::EndpointStopping => ConnectingError::EndpointStopping,
-            ConnectError::TooManyConnections => ConnectingError::TooManyConnections,
-            ConnectError::InvalidDnsName(name) => ConnectingError::InvalidDnsName(name),
+            ConnectError::TooManyConnections => {
+                ConnectingError::TooManyConnections
+            }
+            ConnectError::InvalidDnsName(name) => {
+                ConnectingError::InvalidDnsName(name)
+            }
             ConnectError::InvalidRemoteAddress(socket_addr) => {
                 ConnectingError::InvalidRemoteAddress(socket_addr)
             }
-            ConnectError::NoDefaultClientConfig => ConnectingError::NoDefaultClientConfig,
-            ConnectError::UnsupportedVersion => ConnectingError::UnsupportedVersion,
+            ConnectError::NoDefaultClientConfig => {
+                ConnectingError::NoDefaultClientConfig
+            }
+            ConnectError::UnsupportedVersion => {
+                ConnectingError::UnsupportedVersion
+            }
         }
     }
 }
@@ -46,8 +61,12 @@ impl From<ConnectError> for ConnectingError {
 impl From<ConnectionError> for ConnectingError {
     fn from(error: ConnectionError) -> Self {
         match error {
-            ConnectionError::VersionMismatch => ConnectingError::VersionMismatch,
-            ConnectionError::TransportError(e) => ConnectingError::TransportError(e),
+            ConnectionError::VersionMismatch => {
+                ConnectingError::VersionMismatch
+            }
+            ConnectionError::TransportError(e) => {
+                ConnectingError::TransportError(e)
+            }
             ConnectionError::ConnectionClosed(close_frame) => {
                 ConnectingError::ConnectionClosed(close_frame)
             }
@@ -90,35 +109,39 @@ struct Endpoint {
 }
 
 impl Endpoint {
-    /// Creates a new QUIC endpoint bound to the given socket address with the given TLS configuration.
+    /// Creates a new QUIC endpoint bound to the given socket address with the
+    /// given TLS configuration.
     pub fn new(
         socket_addr: SocketAddr,
         key: &[u8],
         cert: &[u8],
     ) -> io::Result<Self> {
         let socket = std::net::UdpSocket::bind(socket_addr)?;
-        let runtime = default_runtime()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "no async runtime found"))?;
-        
+        let runtime = default_runtime().ok_or_else(|| {
+            io::Error::new(io::ErrorKind::Other, "no async runtime found")
+        })?;
+
         let (client_tls, server_tls) = Self::tls_config(cert, key).unwrap();
-        
+
         // shared transport configuration for the server and client sides
         // this is the default config with the BBR congestion controller enabled
         let mut transport_config = quinn::TransportConfig::default();
         let bbr_config = quinn::congestion::BbrConfig::default();
         transport_config.congestion_controller_factory(Arc::new(bbr_config));
         let transport_config = Arc::new(transport_config);
-        
-        let mut server_config = quinn::ServerConfig::with_crypto(Arc::new(server_tls));
+
+        let mut server_config =
+            quinn::ServerConfig::with_crypto(Arc::new(server_tls));
         server_config.transport_config(transport_config.clone());
-        
+
         let mut client_config = quinn::ClientConfig::new(Arc::new(client_tls));
         client_config.transport_config(transport_config);
-        
+
         let config = quinn::EndpointConfig::default();
-        let mut ep = quinn::Endpoint::new(config, Some(server_config), socket, runtime)?;
+        let mut ep =
+            quinn::Endpoint::new(config, Some(server_config), socket, runtime)?;
         ep.set_default_client_config(client_config);
-        
+
         let ep = Arc::new(ep);
         tokio::spawn(Self::acceptor(ep.clone()));
         Ok(Endpoint { ep })
@@ -132,71 +155,105 @@ impl Endpoint {
         config: quinn::ServerConfig,
     ) -> Result<(), ConnectingError> {
         let axon = self.ep.connect(addr, server_name)?.await?;
-        
+
         // heartbeat stream
-        let (send, receive) = axon.open_bi().await?;
-        
-        // axons support multiple streams which are prioritized and handled asynchronously
-        // neuron maintains streams in this order of priority:
-        // 0. heartbeat stream (highest priority)
+        // send is moved to a basic task that sends heartbeats at a regular
+        // interval receive is moved to a more advanced task that
+        // handles measuring all axons' quality which is responsible for
+        // triggering leader elections and optimising routing
+        let (hb_send, hb_receive) = axon.open_bi().await?;
+        // control streams manage higher level neuron metadata and routing info,
+        // such as a add/remove router, add/remove region, etc
+        // this control channel isn't generally very busy, so these two are
+        // handled in the same task
+        let (ctrl_send, ctrl_receive) = axon.open_bi().await?;
+
+        // axons support multiple {bi/uni}directional streams which are
+        // prioritized and handled asynchronously neuron maintains
+        // streams in this order of priority:
+        // 0. heartbeat stream (bi-directional)
         //    - responsible for sending and receiving heartbeat messages
         //    - measure latency and assist axon quality and routing information
-        // 1. control stream
+        // 1. control stream (bi-directional)
         //    - responsible for neuron's network metadata
         //    - such as new router information
-        // 2... dynamically created per-partition data streams (lowest priority)
-        //    - responsible for sending application data
-        //    - created dynamically as they are inexpensive to create
-        //      and allow for efficient parallelism
+        // 2... dynamically created per-partition data streams (uni-directional
+        // due to number of partitions)
+        //    - responsible for handling writing and reading
+        //    - created dynamically as they are inexpensive to create and allow
+        //      for efficient parallelism
+        //    - may reach up to 65536 streams in total per axon
+        //    - each stream is assigned a partition
+        //    - each partition handler owns its own stream - it won't block
+        //      other partitions
+        //    - each partition has a pair of unidirectional channels for each
+        //      axon
+        //
         Ok(())
     }
-    
+
     /// Accepts incoming axons and spawns tasks to handle them.
-    /// This will run until the endpoint is shut down, so it should be spawned in a dedicated task.
-    pub async fn acceptor(ep: Arc<quinn::Endpoint>) -> Result<(), ConnectingError> {
+    /// This will run until the endpoint is shut down, so it should be spawned
+    /// in a dedicated task.
+    pub async fn acceptor(
+        ep: Arc<quinn::Endpoint>,
+    ) -> Result<(), ConnectingError> {
         while let Some(conn) = ep.accept().await {
             Axon::new(conn.await?);
         }
         Ok(())
     }
-    
+
     /// Returns rustls configurations using SKI's root CA.
-    fn tls_config(cert: &[u8], key: &[u8]) -> Result<(rustls::ClientConfig, rustls::ServerConfig), rustls::Error> {
+    fn tls_config(
+        cert: &[u8],
+        key: &[u8],
+    ) -> Result<(rustls::ClientConfig, rustls::ServerConfig), rustls::Error>
+    {
         let root_ca = Certificate::read_bytes(SKI_ROOT_CA)?;
         let mut root_certs = rustls::RootCertStore::empty();
         root_certs.add(&root_ca)?;
-        
+
         let cert = Certificate::read_bytes(cert)?;
         let key = {
-            let mut keys = rustls_pemfile::pkcs8_private_keys(&mut BufReader::new(Cursor::new(key))).unwrap();
+            let mut keys = rustls_pemfile::pkcs8_private_keys(
+                &mut BufReader::new(Cursor::new(key)),
+            )
+            .unwrap();
             if keys.len() != 1 {
                 panic!("expected exactly one private key");
             }
             keys.remove(0)
         };
-        
+
         let client_config = rustls::ClientConfig::builder()
             .with_safe_defaults()
             .with_root_certificates(root_certs.clone())
-            .with_client_auth_cert(vec![cert.clone()], PrivateKey(key.clone()))?;
-        
+            .with_client_auth_cert(
+                vec![cert.clone()],
+                PrivateKey(key.clone()),
+            )?;
+
         let server_config = rustls::ServerConfig::builder()
             .with_safe_defaults()
-            .with_client_cert_verifier(AllowAnyAuthenticatedClient::new(root_certs).boxed())
+            .with_client_cert_verifier(
+                AllowAnyAuthenticatedClient::new(root_certs).boxed(),
+            )
             .with_single_cert(vec![cert], PrivateKey(key))?;
-        
+
         Ok((client_config, server_config))
     }
 }
 
-/// Axons are QUIC backbone links that connect Aciedo's global infrastructure together.
+/// Axons are QUIC backbone links that connect Aciedo's global infrastructure
+/// together.
 struct Axon(quinn::Connection);
 
 impl Axon {
     fn new(conn: quinn::Connection) -> Self {
         Axon(conn)
     }
-    
+
     fn id(&self) -> usize {
         self.0.stable_id()
     }
